f19865f4b06dd39ae35e57763e70663603db9028
luissalazar <l_salazar85@icloud.com>
2021-10-15 19:56:45 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const persons = [
+    {
+        name: 'Luis',
+        phone: '1158632828',
+        street: 'Bahia Blanca 1337',
+        city: 'Buenos Aires',
+        id: '3d94650'
+    },
+    {
+        name: 'Alfredo',
+        phone: '117862342',
+        street: 'Araoz 5678',
+        city: 'Buenos Aires',
+        id: '3d94650'
+    },
+    {
+        name: 'Rosa',
+        phone: '4321995',
+        street: 'Eleodoro Lobos 285',
+        city: 'Buenos Aires',
+        id: '3d94650'
+    }
+]
